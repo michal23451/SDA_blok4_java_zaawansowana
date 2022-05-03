63393b4e4a8200d7ed3653bf8f91032dcebf5372
Michał Konowalski <konowalski.michal@gmail.com>
2022-05-03 18:50:40 +0200

    homework

---
+++ src/day2/homework/Address.java
@@ -0,0 +1,25 @@
+package day2.homework;
+
+public class Address {
+    private String place;
+    private String streetAndNumber;
+
+    public Address(String place, String streetAndNumber) {
+        this.place = place;
+        this.streetAndNumber = streetAndNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "place='" + place + '\'' +
+                ", streetAndNumber='" + streetAndNumber + '\'' +
+                '}';
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+
+}
